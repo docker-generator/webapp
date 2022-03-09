d043d08ac25bbaeaf51c5d81f9dcf433a2a2afbb
AymericM <aymeric.moehn@gmail.com>
2022-03-09 18:02:01 +0100

    refactor(home): split into multiple components

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
 import { MainContext } from 'stores'
-import { HomeListItem, IconInput } from 'components'
-import { buttons, containers, texts, inputs } from 'styles'
 import { useNavigate } from 'react-router-dom'
-import ic_search from 'assets/images/ic-search.svg'
+import { HomeListItem, HomeHeader } from 'components'
+import { buttons, containers, texts } from 'styles'
 
 export default function Home() {
     const { state, actions } = useContext(MainContext)
@@ -16,29 +15,7 @@ export default function Home() {
 
     return (
         <containers.main style={{ marginTop: '45px' }}>
-            <containers.row_wide noPadding style={{ marginBottom: '50px' }}>
-                <containers.col_left>
-                    <texts.base
-                        as={'h1'}
-                        size={texts.sizes.title_regular}
-                        weight={texts.weights.bold}
-                        style={{ marginBottom: '20px' }}
-                    >My setups</texts.base>
-
-                    <IconInput icon={ic_search}>
-                        <inputs.base
-                            type={'text'}
-                            name={'search'}
-                            placeholder={'Search'}
-                            style={{
-                                width: '100%',
-                                maxWidth: '375px',
-                            }}
-                        />
-                    </IconInput>
-                </containers.col_left>
-                <buttons.success onClick={createNewConfig}>Create a new configuration</buttons.success>
-            </containers.row_wide>
+            <HomeHeader createNewConfig={createNewConfig} />
 
             {state.data.length ? (
                 state.data.map(item => <HomeListItem key={`li_${item.id}`} {...item} />)
