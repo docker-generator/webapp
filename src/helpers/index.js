28b2c9a37fbcc6508ed879e33277de2361fbebc1
AymericM <aymeric.moehn@gmail.com>
2022-03-10 16:59:23 +0100

    feat(editor): display preview

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 export { default as capitalizeFirstLetter } from './capitalizeFirstLetter'
 export { default as getNewConfig } from './getNewConfig'
 export { default as getNewDataFromType } from './getNewDataFromType'
+export { default as formatToYaml } from './formatToYaml'
 export { default as mockData } from './mockData'
